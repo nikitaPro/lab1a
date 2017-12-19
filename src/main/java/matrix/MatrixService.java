3719862144376ddad5e3f6e613bba8945a14d7c8
nikitaPro <nikita_prokopenko@yahoo.com>
2017-12-19 21:01:43 +0300

    Checkstyle

--- src/main/java/matrix/MatrixService.java
+++ src/main/java/matrix/MatrixService.java
@@ -1,62 +1,81 @@
 package matrix;
+
 import org.jetbrains.annotations.NotNull;
+
 public class MatrixService {
-	
-	/**
-	 * The elements of the main diagonal of a square matrix will be sorted in ascending order.
-	 * 
-	 * @param matrix a square matrix of int values which will be sorted.
-	 * @throws IllegalArgumentException if matrix not square
-	 * */
-	public void sortDiagonal(@NotNull int[][] matrix) {
-		if (!isSquare(matrix))
-			throw new IllegalArgumentException("Only square matrix permitted");
-		int size = matrix.length;
-		int[] diag = new int[size]; 
-		for (int i = 0; i < size; i++) diag[i] = matrix[i][i];
-		int buf;
-		boolean already;
-		for (int i = 0; i < diag.length; i++) {
-			already = true;
-			for (int j = 0; j < diag.length - 1 - i; j++) {
-				if (diag[j] > diag[j+1]){
-					buf = diag[j];
-					diag[j] = diag[j+1];
-					diag[j + 1] = buf;
-					already = false;
-				}
-			}
-			if (already) break;
-		}
-		for (int i = 0; i < size; i++) matrix[i][i] = diag[i];
-	}
-	/**
-	 * Mirror reflect a square matrix elements under the main diagonal, 
-	 * by elements which above the main diagonal.
-	 * 
-	 * @param matrix a square matrix of int values which will reflected.
-	 * @throws IllegalArgumentException if matrix not square
-	 * */
-	public void mirror(@NotNull int[][] matrix) {
-		if (!isSquare(matrix))
-			throw new IllegalArgumentException("Only square matrix permitted");
-		int size = matrix.length;
-		for (int i = 0; i < size; i++) {
-			for (int j = 1 + i; j < size; j++) {
-				matrix[i][j] = matrix[j][i];
-			}
-		}
-	}
-	/**
-	 * Returns whether matrix is a square.
-	 * 
-	 * @param matrix  a square matrix of int values which will reflected.
-	 * @return        true, if the matrix is a square, false otherwise
-	 * */
-	private boolean isSquare(int[][] matrix) {
-		int rowSize = matrix.length;
-		boolean isEqual = true;
-		for (int i = 0; i < rowSize; i++) isEqual &= rowSize == matrix[i].length;
-		return isEqual;
-	}
+
+  /**
+   * The elements of the main diagonal of a square matrix will be sorted in ascending order.
+   * 
+   * @param matrix
+   *          a square matrix of int values which will be sorted.
+   * @throws IllegalArgumentException
+   *           if matrix not square
+   */
+  public void sortDiagonal(@NotNull int[][] matrix) {
+    if (!isSquare(matrix)) {
+      throw new IllegalArgumentException("Only square matrix permitted");
+    }
+    int size = matrix.length;
+    int[] diag = new int[size];
+    for (int i = 0; i < size; i++) {
+      diag[i] = matrix[i][i];
+    }
+    int buf;
+    boolean already;
+    for (int i = 0; i < diag.length; i++) {
+      already = true;
+      for (int j = 0; j < diag.length - 1 - i; j++) {
+        if (diag[j] > diag[j + 1]) {
+          buf = diag[j];
+          diag[j] = diag[j + 1];
+          diag[j + 1] = buf;
+          already = false;
+        }
+      }
+      if (already) {
+        break;
+      }
+    }
+    for (int i = 0; i < size; i++) {
+      matrix[i][i] = diag[i];
+    }
+  }
+
+  /**
+   * Mirror reflect a square matrix elements under the main diagonal, by elements which above the
+   * main diagonal.
+   * 
+   * @param matrix
+   *          a square matrix of int values which will reflected.
+   * @throws IllegalArgumentException
+   *           if matrix not square
+   */
+  public void mirror(@NotNull int[][] matrix) {
+    if (!isSquare(matrix)) {
+      throw new IllegalArgumentException("Only square matrix permitted");
+    }
+    int size = matrix.length;
+    for (int i = 0; i < size; i++) {
+      for (int j = 1 + i; j < size; j++) {
+        matrix[i][j] = matrix[j][i];
+      }
+    }
+  }
+
+  /**
+   * Returns whether matrix is a square.
+   * 
+   * @param matrix
+   *          a square matrix of int values which will reflected.
+   * @return true, if the matrix is a square, false otherwise
+   */
+  private boolean isSquare(int[][] matrix) {
+    int rowSize = matrix.length;
+    boolean isEqual = true;
+    for (int i = 0; i < rowSize; i++) {
+      isEqual &= rowSize == matrix[i].length;
+    }
+    return isEqual;
+  }
 }
--- src/test/java/matrix/MatrixServiceTest.java
+++ src/test/java/matrix/MatrixServiceTest.java
@@ -6,168 +6,143 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MatrixServiceTest {
-	private int[][] squareExpectedMatrix= new int[][]{
-		{1, 1, 1, 1},
-		{0, 2, 1, 1},
-		{0, 0, 3, 1},
-		{0, 0, 0, 4}};
-	private MatrixService matrix;
-	@Before
-	public void setUp() throws Exception {
-		matrix=new MatrixService();
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testSortDiagonalMatrix10by0() {
-		int[][] matrix10x0 = new int[10][0];
-		matrix.sortDiagonal(matrix10x0);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testSortDiagonalTriangularMatrix() {
-		int[][] triangularMatrix = new int[][]{
-			{2},
-			{0, 4},
-			{0, 0, 3},
-			{0, 0, 0, 1}};
-		matrix.sortDiagonal(triangularMatrix);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testSortDiagonalRectangularMatrix1() {
-		int[][] rectangularMatrix1 = new int[][]{
-			{2, 1, 1, 1, 1},
-			{0, 4, 1, 1, 1},
-			{0, 0, 3, 1, 1},
-			{0, 0, 0, 1, 1}};
-		matrix.sortDiagonal(rectangularMatrix1);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testSortDiagonalRectangularMatrix2() {
-		int[][] rectangularMatrix2 = new int[][]{
-			{2, 1, 1, 1},
-			{0, 4, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 1},
-			{0, 0, 0, 0}};
-		matrix.sortDiagonal(rectangularMatrix2);
-	}
-	@Test 
-	public void testSortDiagonalSquareRandomMatrix() {
-		int[][] squareRandomMatrix = new int[][]{
-			{2, 1, 1, 1},
-			{0, 4, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 1}};
-		matrix.sortDiagonal(squareRandomMatrix);
-		assertArray2DEquals(squareExpectedMatrix, squareRandomMatrix);
-	}
-	@Test 
-	public void testSortDiagonalSquareDescendingMatrix() {
-		int[][] squareDescendingMatrix = new int[][]{
-			{4, 1, 1, 1},
-			{0, 3, 1, 1},
-			{0, 0, 2, 1},
-			{0, 0, 0, 1}};
-		matrix.sortDiagonal(squareDescendingMatrix);
-		assertArray2DEquals(squareExpectedMatrix, squareDescendingMatrix);
-	}
-	@Test 
-	public void testSortDiagonalSquareAscendingMatrix2() {
-		int[][] squareAscendingMatrix2 = new int[][]{
-			{1, 1, 1, 1},
-			{0, 2, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 4}};
-		matrix.sortDiagonal(squareAscendingMatrix2);
-		assertArray2DEquals(squareExpectedMatrix, squareAscendingMatrix2);
-	}
-	@Test 
-	public void testSortDiagonalZiroMatrix() {
-		int[][] ziroMatrix = new int[0][0];
-		matrix.sortDiagonal(ziroMatrix);
-	}
-	@Test 
-	public void testSortDiagonalMatrix1x1() {
-		int[][] matrix1x1 = new int[][]{{5}};
-		matrix.sortDiagonal(matrix1x1);
-		assertArrayEquals(new int[][]{{5}}, matrix1x1);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testMirrorTriangularMatrix() {
-		int[][] triangularMatrix = new int[][]{
-			{2},
-			{0, 4},
-			{0, 0, 3},
-			{0, 0, 0, 1}};
-		matrix.mirror(triangularMatrix);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testMirrorRectangularMatrix1() {
-		int[][] rectangularMatrix1 = new int[][]{
-			{2, 1, 1, 1, 1},
-			{0, 4, 1, 1, 1},
-			{0, 0, 3, 1, 1},
-			{0, 0, 0, 1, 1}};
-		matrix.mirror(rectangularMatrix1);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testMirrorRectangularMatrix2() {
-		int[][] rectangularMatrix2 = new int[][]{
-			{2, 1, 1, 1},
-			{0, 4, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 1},
-			{0, 0, 0, 0}};
-		matrix.mirror(rectangularMatrix2);
-	}
-	@Test (expected = IllegalArgumentException.class)
-	public void testMirrorMatrix10by0() {
-		int[][] matrix10x0 = new int[10][0];
-		matrix.mirror(matrix10x0);
-	}
-	@Test
-	public void testMirrorSquareRandomMatrix() {
-		int[][] squareRandomMatrix = new int[][]{
-			{2, 1, 1, 1},
-			{0, 4, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 1}};
-		matrix.mirror(squareRandomMatrix);
-		int[][] squareMirrorRandomMatrix = new int[][]{
-			{2, 0, 0, 0},
-			{0, 4, 0, 0},
-			{0, 0, 3, 0},
-			{0, 0, 0, 1}};
-		assertArray2DEquals(squareMirrorRandomMatrix, squareRandomMatrix);
-	}
-	@Test
-	public void testMirrorSquareAscendingMatrix2() {
-		int[][] squareAscendingMatrix2 = new int[][]{
-			{1, 1, 1, 1},
-			{0, 2, 1, 1},
-			{0, 0, 3, 1},
-			{0, 0, 0, 4}};
-		matrix.mirror(squareAscendingMatrix2);
-		int[][] squareMirrorAscendingMatrix = new int[][]{
-			{1, 0, 0, 0},
-			{0, 2, 0, 0},
-			{0, 0, 3, 0},
-			{0, 0, 0, 4}};
-		assertArray2DEquals(squareMirrorAscendingMatrix, squareAscendingMatrix2);
-	}
-	@Test
-	public void testMirrorMatrix1x1() {
-		int[][] matrix1x1 = new int[][]{{5}};
-		matrix.mirror(matrix1x1);
-		assertArrayEquals(new int[][]{{5}}, matrix1x1);
-	}
-	@Test
-	public void testMirrorZiroMatrix() {
-		int[][] ziroMatrix = new int[0][0];
-		matrix.mirror(ziroMatrix);
-	}
-	private void assertArray2DEquals(int array1[][], int array2[][]){
-		if (array1.length != array2.length) fail();
-		for (int i = 0; i < array1.length; i++) {
-			assertArrayEquals(array1, array2);
-		}
-	}
+  private int[][] squareExpectedMatrix = new int[][] { { 1, 1, 1, 1 }, { 0, 2, 1, 1 },
+      { 0, 0, 3, 1 }, { 0, 0, 0, 4 } };
+  private MatrixService matrix;
+
+  @Before
+  public void setUp() throws Exception {
+    matrix = new MatrixService();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSortDiagonalMatrix10by0() {
+    int[][] matrix10x0 = new int[10][0];
+    matrix.sortDiagonal(matrix10x0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSortDiagonalTriangularMatrix() {
+    int[][] triangularMatrix = new int[][] { { 2 }, { 0, 4 }, { 0, 0, 3 }, { 0, 0, 0, 1 } };
+    matrix.sortDiagonal(triangularMatrix);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSortDiagonalRectangularMatrix1() {
+    int[][] rectangularMatrix1 = new int[][] { { 2, 1, 1, 1, 1 }, { 0, 4, 1, 1, 1 },
+        { 0, 0, 3, 1, 1 }, { 0, 0, 0, 1, 1 } };
+    matrix.sortDiagonal(rectangularMatrix1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSortDiagonalRectangularMatrix2() {
+    int[][] rectangularMatrix2 = new int[][] { { 2, 1, 1, 1 }, { 0, 4, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 1 }, { 0, 0, 0, 0 } };
+    matrix.sortDiagonal(rectangularMatrix2);
+  }
+
+  @Test
+  public void testSortDiagonalSquareRandomMatrix() {
+    int[][] squareRandomMatrix = new int[][] { { 2, 1, 1, 1 }, { 0, 4, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 1 } };
+    matrix.sortDiagonal(squareRandomMatrix);
+    assertArray2DEquals(squareExpectedMatrix, squareRandomMatrix);
+  }
+
+  @Test
+  public void testSortDiagonalSquareDescendingMatrix() {
+    int[][] squareDescendingMatrix = new int[][] { { 4, 1, 1, 1 }, { 0, 3, 1, 1 }, { 0, 0, 2, 1 },
+        { 0, 0, 0, 1 } };
+    matrix.sortDiagonal(squareDescendingMatrix);
+    assertArray2DEquals(squareExpectedMatrix, squareDescendingMatrix);
+  }
+
+  @Test
+  public void testSortDiagonalSquareAscendingMatrix2() {
+    int[][] squareAscendingMatrix2 = new int[][] { { 1, 1, 1, 1 }, { 0, 2, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 4 } };
+    matrix.sortDiagonal(squareAscendingMatrix2);
+    assertArray2DEquals(squareExpectedMatrix, squareAscendingMatrix2);
+  }
+
+  @Test
+  public void testSortDiagonalZiroMatrix() {
+    int[][] ziroMatrix = new int[0][0];
+    matrix.sortDiagonal(ziroMatrix);
+  }
+
+  @Test
+  public void testSortDiagonalMatrix1x1() {
+    int[][] matrix1x1 = new int[][] { { 5 } };
+    matrix.sortDiagonal(matrix1x1);
+    assertArrayEquals(new int[][] { { 5 } }, matrix1x1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMirrorTriangularMatrix() {
+    int[][] triangularMatrix = new int[][] { { 2 }, { 0, 4 }, { 0, 0, 3 }, { 0, 0, 0, 1 } };
+    matrix.mirror(triangularMatrix);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMirrorRectangularMatrix1() {
+    int[][] rectangularMatrix1 = new int[][] { { 2, 1, 1, 1, 1 }, { 0, 4, 1, 1, 1 },
+        { 0, 0, 3, 1, 1 }, { 0, 0, 0, 1, 1 } };
+    matrix.mirror(rectangularMatrix1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMirrorRectangularMatrix2() {
+    int[][] rectangularMatrix2 = new int[][] { { 2, 1, 1, 1 }, { 0, 4, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 1 }, { 0, 0, 0, 0 } };
+    matrix.mirror(rectangularMatrix2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMirrorMatrix10by0() {
+    int[][] matrix10x0 = new int[10][0];
+    matrix.mirror(matrix10x0);
+  }
+
+  @Test
+  public void testMirrorSquareRandomMatrix() {
+    int[][] squareRandomMatrix = new int[][] { { 2, 1, 1, 1 }, { 0, 4, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 1 } };
+    matrix.mirror(squareRandomMatrix);
+    int[][] squareMirrorRandomMatrix = new int[][] { { 2, 0, 0, 0 }, { 0, 4, 0, 0 }, { 0, 0, 3, 0 },
+        { 0, 0, 0, 1 } };
+    assertArray2DEquals(squareMirrorRandomMatrix, squareRandomMatrix);
+  }
+
+  @Test
+  public void testMirrorSquareAscendingMatrix2() {
+    int[][] squareAscendingMatrix2 = new int[][] { { 1, 1, 1, 1 }, { 0, 2, 1, 1 }, { 0, 0, 3, 1 },
+        { 0, 0, 0, 4 } };
+    matrix.mirror(squareAscendingMatrix2);
+    int[][] squareMirrorAscendingMatrix = new int[][] { { 1, 0, 0, 0 }, { 0, 2, 0, 0 },
+        { 0, 0, 3, 0 }, { 0, 0, 0, 4 } };
+    assertArray2DEquals(squareMirrorAscendingMatrix, squareAscendingMatrix2);
+  }
+
+  @Test
+  public void testMirrorMatrix1x1() {
+    int[][] matrix1x1 = new int[][] {{5}};
+    matrix.mirror(matrix1x1);
+    assertArrayEquals(new int[][] { { 5 } }, matrix1x1);
+  }
+
+  @Test
+  public void testMirrorZiroMatrix() {
+    int[][] ziroMatrix = new int[0][0];
+    matrix.mirror(ziroMatrix);
+  }
+
+  private void assertArray2DEquals(int[][] array1, int[][] array2) {
+    if (array1.length != array2.length) {
+      fail();
+    }
+    for (int i = 0; i < array1.length; i++) {
+      assertArrayEquals(array1, array2);
+    }
+  }
 }
